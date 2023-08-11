2f9cd8208da1338a78f6cd805f7bb014f86cb080
Yab1 <yabilisanu@gmail.com>
2023-08-11 14:14:47 +0300

    Config: Configured Gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,12 @@
-const { src, dest, watch, series } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const purgecss = require('gulp-purgecss');
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src('sass/**/*.scss')
-    .pipe(sass())
-    .pipe(purgecss({ content: ['*.html'] }))
-    .pipe(dest('css'));
+  return src("sass/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(['sass/**/*.scss', '*.html'], buildStyles);
+  watch(["sass/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
