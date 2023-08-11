9cf88bd6201c6f6d113f6e2212950e8214f322bd
Yab1 <yabilisanu@gmail.com>
2023-08-11 21:02:29 +0300

    Config: Configured PurgeCSS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("sass/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("sass/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["dist/*.html"] }))
+    .pipe(dest("css"));
 }
 
 function watchTask() {
