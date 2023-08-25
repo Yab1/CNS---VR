e50acebd9be657974a4eada3916dd80960385136
Yab1 <yabilisanu@gmail.com>
2023-08-25 16:53:43 +0300

    Setup Copy webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {
+    main: "./src/index.js",
+    amphi: "./windows/Amphi/Amphi.html",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
-  plugins: [new Dotenv()],
+  plugins: [
+    new Dotenv(),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/windows/Amphi", to: "windows/Amphi" },
+        // ... other patterns
+      ],
+    }),
+  ],
   watch: true,
 };
